8fa7ee166fb84b19063057bfeba2fbd2c7758f94
Geert Custers <geert.aj.custers@gmail.com>
2019-05-14 15:39:35 +0200

    Think we found it

--- travis/travis_osx.sh
+++ travis/travis_osx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 brew install cereal mysql-connector-c libevent openssl yaml-cpp argon2 pkg-config imagemagick
-ls /usr/local/Cellar/qt
-export CMAKE_PREFIX_PATH=/usr/local/Cellar/qt/5.12.3
+export CMAKE_PREFIX_PATH=/usr/local/Cellar/qt/5.12.2
