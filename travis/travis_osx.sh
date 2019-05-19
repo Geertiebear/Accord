51cd38a17125212160141402a3b14f885b1ff8f4
Geert Custers <geert.aj.custers@gmail.com>
2019-05-19 09:44:16 +0200

    ls pkgconfig dir

--- travis/travis_osx.sh
+++ travis/travis_osx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 brew install cereal mysql-connector-c libevent openssl yaml-cpp argon2 pkg-config imagemagick
+ls /usr/local/share/pkgconfig
 pkg-config --libs argon2
 pkg-config --libs libargon2
